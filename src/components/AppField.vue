e4e5703b38266990a92a0fc604e936d38fd689b1
kfil <koafil@gmail.com>
2022-11-20 19:37:47 +0300

    анимации появления и вращения иконок

--- src/components/AppField.vue
+++ src/components/AppField.vue
@@ -2,18 +2,14 @@
   <div>
     <label class="form-label">{{ title }}
       <transition appear
-        enter-active-class="animate__animated animate__fadeIn"
+                  name="fade"
       >
-      <template v-if="activated">
-        <transition
-          mode="out-in"
-          enter-active-class="animate__animated animate__flipInY"
-          leave-active-class="animate__animated animate__flipOutY"
-        >
-          <b-icon-check-circle class="text-success" v-if="valid"/>
-          <b-icon-exclamation-circle class="text-danger" v-else/>
-        </transition>
-      </template>
+        <span v-if="activated">
+          <transition name="rotate" mode="out-in">
+            <b-icon-check-circle class="text-success" v-if="valid" />
+            <b-icon-exclamation-circle class="text-danger" v-else />
+          </transition>
+        </span>
       </transition>
     </label>
     <input type="text" class="form-control"
@@ -59,4 +55,42 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.fade-enter-active {
+  animation: fadeIn 0.5s;
+}
+
+.rotate-enter-active {
+  animation: rotateIn 0.2s;
+}
+
+.rotate-leave-active {
+  animation: rotateOut 0.2s;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes rotateIn {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0);
+  }
+}
+
+@keyframes rotateOut {
+  from {
+    transform: rotateY(0);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'animate.css/animate.min.css'
+// import 'animate.css/animate.min.css'
